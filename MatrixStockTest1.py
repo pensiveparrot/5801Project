6637c805e563bc351b4371b04f017bb52530502a
PensiveParrot <54560136+pensiveparrot@users.noreply.github.com>
2020-09-20 19:35:52 -0400

    testing yfinance
    testing yfinance python module.
    
    install with command:
    pip install yfinance
    lxml is required to run the script

--- MatrixStockTest1.py
+++ MatrixStockTest1.py
@@ -1,20 +1,8 @@
-from alpha_vantage.alphavantage import AlphaVantage
-from alpha_vantage.timeseries import TimeSeries
-from alpha_vantage.techindicators import TechIndicators
-from alpha_vantage.sectorperformance import SectorPerformances
-from alpha_vantage.foreignexchange import ForeignExchange
-from alpha_vantage.fundamentaldata import FundamentalData
-
-from pandas import DataFrame as df, Timestamp
-
-import unittest
-import sys
+import yfinance as yfi
 import os
-import requests_mock
-import numpy
-import time
-
 def main():
-    print("A")
+    tsla=yfi.Ticker("TSLA")
+    history=tsla.history(period="1d")
+    print(history)
     os.system("pause")
 main()
--- alpha_vantage/async_support/sectorperformance.py
+++
@@ -1,1 +0,0 @@
-../sectorperformance.py
