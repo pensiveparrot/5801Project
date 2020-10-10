7935c27250e9b5e5064714fcdcfdf69422b47853
PensiveParrot <54560136+pensiveparrot@users.noreply.github.com>
2020-10-10 00:06:00 -0400

    added appjar and tickers

--- MatrixStockTest1.py
+++ MatrixStockTest1.py
@@ -1,8 +1,27 @@
 import yfinance as yfi
 import os
+from appJar import gui
+
+
+
+
 def main():
-    tsla=yfi.Ticker("TSLA")
-    history=tsla.history(period="1d")
-    print(history)
-    os.system("pause")
+    # stock1=input("Enter stock ticker 1")
+    # stock2=input("Enter stock ticker 2")
+
+    #history=tsla.history(period="1d")
+    app = gui("Working with yfinance","640x480")
+    tickers = listmaker()
+    app.addLabelAutoEntry("stock1",tickers)
+    def submit():
+        data=app.getEntry("stock1")
+        print(data)
+    app.setEntrySubmitFunction("stock1",submit)
+
+     #print(history)
+    app.go()
+def listmaker():
+    with open('names.txt', 'r') as f:
+        myNames = [line.strip() for line in f]
+    return myNames
 main()
